5380a2c8ad7c011427367f452718efc6d16ee8f4
Vladimir Borisov <1mt-2@mail.ru>
2022-12-14 18:08:51 +0300

    refactor: paginator

--- src/Components/Common/Paginator.tsx
+++ src/Components/Common/Paginator.tsx
@@ -11,20 +11,21 @@ type propsType = {
 
 const Paginator = (props: propsType) => {
 
-    let pagesCount: number = Math.ceil(props.totalUsersCount / props.pageSize)
-
-    let pages: Array<number> = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
+    // let pagesCount: number = Math.ceil(props.totalUsersCount / props.pageSize)
+    //
+    // let pages: Array<number> = [];
+    // for (let i = 1; i <= pagesCount; i++) {
+    //     pages.push(i)
+    // }
 
 
     return <div>
-            {pages.map(p => {
-                return <span onClick={(event) => {
-                    props.onPageChange(p)
-                }} className={props.currentPage === p ? s.selectedPage : ''}>{p}</span>
-            })}
+            {/*{pages.map(p => {*/}
+            {/*    return <span onClick={(event) => {*/}
+            {/*        props.onPageChange(p)*/}
+            {/*    }} className={props.currentPage === p ? s.selectedPage : ''}>{p}</span>*/}
+            {/*})}*/}
+        <Paginator onPageChange={props.onPageChange} totalUsersCount={props.totalUsersCount} pageSize={props.pageSize} currentPage={props.currentPage} />
         </div>
         
 }
